8769a22ff674e9030e1b33ecdf63eef97c44e939
Jakub Ślawski <kubaslawski.it@gmail.com>
2025-03-10 15:55:10 +0100

    Added tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
         icon: true,
       },
     }),
+    tailwindcss(),
   ],
 });
